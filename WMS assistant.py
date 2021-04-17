d3dbbc963d8496aa1d3e8b7a799f95724d0b03e2
Abhishek Potekar <72246796+Madmaxcoder2612@users.noreply.github.com>
2021-04-17 12:56:02 +0530

    Update WMS assistant.py

--- WMS assistant.py
+++ WMS assistant.py
@@ -9,3 +9,16 @@ engine.setProperty('voices',voices[0].id)
 def speak(audio):
   engine.say(audio)
   engine.runAndWait()
+  
+  def WishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning! Sir")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon! Sir")
+    
+    else:
+        speak("Good Evening! Sir")
+
+    speak("I am JARVIS. Please tell me, How may I help you ")
