f83bba6c95c217e75fd675817fb19e732d644de5
Abhishek Potekar <72246796+Madmaxcoder2612@users.noreply.github.com>
2021-04-17 11:44:33 +0530

    Add files via upload

---
+++ WMS assistant.py
@@ -0,0 +1,11 @@
+import pyttsx3
+import speech_recognition as sr
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+#print(voices[0].id)
+engine.setProperty('voices',voices[0].id)
+
+def speak(audio):
+  engine.say(audio)
+  engine.runAndWait()
